c008da6862326dda47a612cc66a0fc9f39b981e0
Michael <michael.wenceslaus@gmail.com>
2019-10-20 20:50:13 +0700

    group-key

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Author: https://github.com/michaelwp
+ * @param {Array, array item index} value, keyIndex
+ * @returns {object}
+ */
+
+/**
+ * This function will grouping array item
+ * into object based on given key
+ */
+const groupKey = (value, keyIndex) => {
+    const result = {};
+    value.forEach((v) => {
+        const vArray = [];
+        v.forEach((item, index) => {
+            if (index !== keyIndex) {
+                vArray.push(item);
+            }
+        });
+        if (result[v[keyIndex]] === undefined) {
+            result[v[keyIndex]] = [vArray];
+        } else {
+            result[v[keyIndex]].push(vArray);
+        }
+    });
+    return result;
+};
+module.exports = groupKey;
+
