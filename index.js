12ff1f620a08e8e9ae8bbd524abdefe9d66d4b27
Duc An <me@clgt.io>
2019-10-21 13:25:11 +0700

    feat: handle invalid input

--- index.js
+++ index.js
@@ -9,24 +9,27 @@
  * into object based on given key
  */
 const objGroup = (value, keyIndex) => {
-    const result = {};
-    value.forEach((v) => {
-        const vArray = [];
-        v.forEach((item, index) => {
-            if (index !== keyIndex) {
-                vArray.push(item);
-            }
-        });
-        if (result[v[keyIndex]] === undefined) {
-            result[v[keyIndex]] = [vArray];
-        } else {
-            result[v[keyIndex]].push(vArray);
-        }
-    });
-    if (result[undefined]) {
-        console.warn("Key Index out of range")
-        return { errorMsg: "Key Index out of range." }
+  if (value.some(v => v.length < 2)) {
+    console.warn('[obj-group-warning]: invalid input data')
+  }
+  const result = {}
+  value.forEach(v => {
+    const vArray = []
+    v.forEach((item, index) => {
+      if (index !== keyIndex) {
+        vArray.push(item)
+      }
+    })
+    if (result[v[keyIndex]] === undefined) {
+      result[v[keyIndex]] = [vArray]
+    } else {
+      result[v[keyIndex]].push(vArray)
     }
-    return result;
-};
-module.exports = objGroup;
+  })
+  if (result[undefined]) {
+    console.warn('Key Index out of range')
+    return { errorMsg: 'Key Index out of range.' }
+  }
+  return result
+}
+module.exports = objGroup
--- test.js
+++ test.js
@@ -1,59 +1,92 @@
-const objGroup = require('./index');
-const assert = require('assert');
+const objGroup = require('./index')
+const assert = require('assert')
 
 //data testing :
 const value = [
-    ['sniper', 'agility', "ranged"],
-    ['axe', 'strength', 'melee'],
-    ['tusk', 'strength', 'melee'],
-    ['crystal maiden', 'intelligence', 'ranged'],
-    ['phantom assasin', 'agility', 'melee'],
-    ['windrunner', 'intelligence', 'ranged']];
+  ['sniper', 'agility', 'ranged'],
+  ['axe', 'strength', 'melee'],
+  ['tusk', 'strength', 'melee'],
+  ['crystal maiden', 'intelligence', 'ranged'],
+  ['phantom assasin', 'agility', 'melee'],
+  ['windrunner', 'intelligence', 'ranged'],
+]
 
+const invalidValue = [
+  ['sniper'],
+  ['axe'],
+  ['tusk'],
+  ['crystal maiden'],
+  ['phantom assasin'],
+  ['windrunner'],
+]
 
 // expected result :
 const expectedRes = {
-    strength: [['axe', 'melee'], ['tusk', 'melee']],
-    agility: [['sniper', 'ranged'], ['phantom assasin', 'melee']],
-    intelligence: [['crystal maiden', 'ranged'], ['windrunner', 'ranged']]
-};
+  strength: [['axe', 'melee'], ['tusk', 'melee']],
+  agility: [['sniper', 'ranged'], ['phantom assasin', 'melee']],
+  intelligence: [['crystal maiden', 'ranged'], ['windrunner', 'ranged']],
+}
 // the actual result :
-const actualRes = objGroup(value, 1);
+const actualRes = objGroup(value, 1)
 
 // compare the type of :
-assert.strictEqual(typeof expectedRes, typeof actualRes, "both type of are object");
+assert.strictEqual(
+  typeof expectedRes,
+  typeof actualRes,
+  'both type of are object',
+)
 
 // iterating the actual result object :
 for (const e in expectedRes) {
+  // compare the type of item object:
+  assert.strictEqual(
+    typeof expectedRes[e],
+    typeof actualRes[e],
+    'both type of are array',
+  )
 
-    // compare the type of item object:
-    assert.strictEqual(typeof expectedRes[e], typeof actualRes[e], "both type of are array");
+  // compare the type of item object length:
+  assert.strictEqual(
+    expectedRes[e].length,
+    actualRes[e].length,
+    'both length are three (3)',
+  )
 
-    // compare the type of item object length:
-    assert.strictEqual(expectedRes[e].length, actualRes[e].length, "both length are three (3)");
+  // iterating the array :
+  expectedRes[e].forEach((eItemArray, eItemArrayIndex) => {
+    // compare the type of item array:
+    assert.strictEqual(
+      typeof eItemArray,
+      typeof actualRes[e][eItemArrayIndex],
+      'both type of are array',
+    )
 
-    // iterating the array :
-    expectedRes[e].forEach((eItemArray, eItemArrayIndex) => {
+    // compare the item array length:
+    assert.strictEqual(
+      eItemArray.length,
+      actualRes[e][eItemArrayIndex].length,
+      'both length are three (2)',
+    )
 
-        // compare the type of item array:
-        assert.strictEqual(typeof eItemArray, typeof actualRes[e][eItemArrayIndex], "both type of are array");
+    // iterating the item array:
+    eItemArray.forEach((eItem, eItemIndex) => {
+      // compare the type of item:
+      assert.strictEqual(
+        typeof eItem,
+        typeof actualRes[e][eItemArrayIndex][eItemIndex],
+        'both type are string',
+      )
 
-        // compare the item array length:
-        assert.strictEqual(eItemArray.length, actualRes[e][eItemArrayIndex].length, "both length are three (2)");
-
-        // iterating the item array:
-        eItemArray.forEach((eItem, eItemIndex) => {
-
-            // compare the type of item:
-            assert.strictEqual(typeof eItem, typeof actualRes[e][eItemArrayIndex][eItemIndex], "both type are string");
-
-            // compare the item
-            assert.strictEqual(eItem, actualRes[e][eItemArrayIndex][eItemIndex], "equal string item");
-
-        });
-
-    });
-
+      // compare the item
+      assert.strictEqual(
+        eItem,
+        actualRes[e][eItemArrayIndex][eItemIndex],
+        'equal string item',
+      )
+    })
+  })
 }
 
-console.log("test running successfully");
+objGroup(invalidValue)
+
+console.log('test running successfully')
