fa93ec3a66453eececd099c838bdbd4301bfc17e
Emad Bin Abid <emad.bin.abid@gmail.com>
2019-10-21 11:55:31 +0500

    Merged final groups if array length === 2.

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ const objGroup = (value, keyIndex) => {
         } else {
             result[v[keyIndex]].push(vArray);
         }
+        
+        // Merge groups if array length === 2
+        if (v.length === 2) {
+            result[v[keyIndex]] = [].concat(...result[v[keyIndex]]);
+        }
     });
     if (result[undefined]) {
         console.warn("Key Index out of range")
